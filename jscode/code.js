820b7d5809e35f55f3a44609a949f3ae428815b9
Akash Reddy G <akash.gillella@gmail.com>
2022-10-02 07:56:42 +0530

    Added contact Form

--- jscode/code.js
+++ jscode/code.js
@@ -31,3 +31,39 @@ function backToTop() {
     document.documentElement.scrollTop = 0;
 }
 /* GO-TO-TOP BUTTON */
+
+
+/* Trying to remove the jotforms branding */
+/* Wont work due to same-origin policy */
+/* JS wont access inside a iframe */
+// contactFormDiv = document.getElementById('contactForm')
+
+// function noneFormFooter() {
+
+//     formFooter = document.querySelector("body > div.formFooter");
+//     console.log(contactFormDiv);
+//     console.log(formFooter);
+//     console.log(formFooter.style);
+//     formFooter.style.display = "none"
+
+// }
+
+
+// // Create an observer instance linked to the callback function
+// const observer = new MutationObserver(noneFormFooter);
+
+// const config = { attributes: true, childList: true, subtree: true };
+
+// // Start observing the target node for configured mutations
+// observer.observe(contactFormDiv, config);
+
+
+/* These old jotforms allow custom CSS, i can change formFooter there itself */
+
+// var doc = document.getElementsByTagName("iframe");
+// doc = document.getElementsByTagName("iframe")[doc.length - 1].contentDocument;
+// var footElem = doc.lastChild.lastChild
+// var newStyle = document.createElement('style')
+// newStyle.innerHTML = ".formFooter {display:none;}";
+// footElem.insertBefore(newStyle, footElem.lastChild)
+
